2c12f044c4bdd002f74021b2dfaad24da39d8514
Bams <bbg2021@gmail.com>
2023-03-20 12:47:33 +0800

    init after removing sub dir

---
+++ frontend/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-4xl font-bold">👋 Hello Laravel!</h1>
+</template>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import axios from "axios";
+
+definePageMeta({
+  layout: "centered",
+});
+
+interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const form = ref({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+});
+
+async function register(payload: RegisterPayload) {
+  const res = await axios.post("/register", payload);
+  console.log(res);
+}
+</script>
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <form @submit.prevent="register(form)">
+      <label>
+        <div>Name</div>
+        <input type="text" v-model="form.name" />
+      </label>
+
+      <label>
+        <div>Email</div>
+        <input type="email" v-model="form.email" />
+      </label>
+
+      <label>
+        <div>Password</div>
+        <input type="password" v-model="form.password" />
+      </label>
+
+      <label>
+        <div>Confirm Password</div>
+        <input type="password" v-model="form.password_confirmation" />
+      </label>
+
+      <button class="btn">Register</button>
+    </form>
+
+    <p>
+      Already have an account?
+      <NuxtLink class="underline text-lime-600" to="/login">Login</NuxtLink>
+    </p>
+  </div>
+</template>
